66e653bc0e6aa346ba3f506c30e5c748805bebd0
Basit ali <160397104+Basit7864@users.noreply.github.com>
2024-11-10 22:24:46 +0500

    Create About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../app/styles/about.css"
+
+const About = () => {
+  return (
+
+    <div id='about' className='about-container'>
+          <h2 className="about-heading" data-aos="zoom-in-up">
+            About Us
+          </h2>
+        <p className="about-text " data-aos="zoom-in-up"> 
+           I am a Graphic Designer and I'm the student of GIAIC <b>(Governor Initiative Artificial Intelligence Course)</b> 
+             to learn Artificial Intelligence , web 3.O , metaverse</p>
+    </div>
+    
+  )
+}
+
+export default About
