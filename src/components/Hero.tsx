6bf224b099bc1dd314fceeb18e3bc3c9b66bc130
Basit ali <160397104+Basit7864@users.noreply.github.com>
2024-11-11 15:55:54 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,20 +6,20 @@ const Hero = () => {
   return (
     <div
   id="hero"
-  className=" min-h-screen bg-no-repeat bg-[url(/web.svg)] bg-cover"
-  style={{ backgroundSize: "160%", backgroundPosition: "left 100px top 10px"}} 
+  className=" min-h-screen bg-no-repeat bg-[url(/web.png)] bg-cover " 
+  style={{ backgroundSize: "40%",backgroundPosition: "left 120px top 165px" }} 
 >
 
       <Navbar />
-      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]" >
         <div className="hidden lg:block"></div>
 
         {/* Changes: Adjusted text size and centering */}
-        <div className="text-[70px] sm:text-[100px] font-bold leading-tight flex justify-center items-center text-aqua">
+        <div className="text-[70px] sm:text-[100px] font-bold leading-tight flex justify-center items-center text-mint">
           <div>
             <p data-aos="zoom-in-up">I'm</p>
             <p data-aos="zoom-in-up">Basit</p>
-            <p data-aos="zoom-in-up">Ali</p>
+            <p data-aos="zoom-in-up">Baloch</p>
           </div>
         </div>
       </div>
